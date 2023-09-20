de768c850540d6809dd0af9fb6f63eb847b85d28
BareMarcP <marc.en.pedersen@gmail.com>
2023-09-20 16:14:28 +0200

    added docker-compose and changes SQL scripts

--- sql/04_InsertData.sql
+++ sql/04_InsertData.sql
@@ -21,3 +21,42 @@ INSERT INTO superheroes_powers (superhero_id, power_id) VALUES
 ((SELECT id from superheroes WHERE name="Batman"), (SELECT id from powers WHERE name="Filthy Rich")),
 ((SELECT id from superheroes WHERE name="Wolverine"), (SELECT id from powers WHERE name="Super Jump")),
 ((SELECT id from superheroes WHERE name="Wolverine"), (SELECT id from powers WHERE name="Retractable Claws"));
+
+
+
+INSERT INTO [dbo].[superheroes] 
+( [name], [alias], [origin])
+VALUES
+( 'Spiderman', 'Spodermon', 'New York City'),
+( 'Batman', 'Bathman', 'Arkham City'),
+( 'Wolverine', 'Wolvedawg', 'Alberta')
+
+GO
+
+INSERT INTO [dbo].[assistants]
+( [name], [superhero_id])
+VALUES
+( 'Uncle Ben', (SELECT id FROM superheroes WHERE name='Spiderman')),
+( 'Alfred', (SELECT id FROM superheroes WHERE name='Batman')),
+( 'Jubilee', (SELECT id FROM superheroes WHERE name='Wolverine'))
+GO
+
+
+INSERT INTO [dbo].[powers]
+( [name], [description])
+VALUES
+( 'Super Jump', 'Hero can jump very high'),
+( 'Filthy Rich', 'Hero has more money than they can spend'),
+( 'Retractable Claws', 'Hero can summon deadly steel claws out of their knucles'),
+( 'Spider Web', 'Hero can spin out webs to travel fast or render enemies harmless')
+
+INSERT INTO [dbo].[superheroes_powers] 
+([superhero_id], [power_id]) 
+VALUES
+((SELECT id from superheroes WHERE name='Spiderman'), (SELECT id from powers WHERE name='Super Jump')),
+((SELECT id from superheroes WHERE name='Spiderman'), (SELECT id from powers WHERE name='Spider Web')),
+((SELECT id from superheroes WHERE name='Batman'), (SELECT id from powers WHERE name='Super Jump')),
+((SELECT id from superheroes WHERE name='Batman'), (SELECT id from powers WHERE name='Filthy Rich')),
+((SELECT id from superheroes WHERE name='Wolverine'), (SELECT id from powers WHERE name='Super Jump')),
+((SELECT id from superheroes WHERE name='Wolverine'), (SELECT id from powers WHERE name='Retractable Claws'));
+GO
