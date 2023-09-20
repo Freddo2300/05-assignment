345d65a472a6b8e304095d1f4a371cc0ab266f51
freddo2300 <fsf96@hotmail.com>
2023-09-20 14:06:58 +0200

    whoops

--- Program.cs
+++ Program.cs
@@ -7,25 +7,6 @@ namespace Chinook
     {
         public static void Main(string[] args)
         {
-            string connectionString
-                = @"
-                Server=localhost; 
-                User ID=fred; 
-                Password=|EUr0m4n96; 
-                Database=chinook";
-
-            using var connection = new MySqlConnection(connectionString);
-
-            var users = connection.Query<string>(@"
-                    SELECT user_name
-                    FROM users
-                    ORDER BY user_id ASC
-                    ");
-
-            foreach (string name in users)
-            {
-                Console.WriteLine($"NAME: {name}");
-            }
 
         }
     }
