2ec5713cf7b50f0d4e953c29864c3205ca430186
mansourhamidi94 <MansourHamidi@hotmail.dk>
2023-09-22 10:31:34 +0200

    started function display customers

--- src/Utils/ConsoleUtils.cs
+++ src/Utils/ConsoleUtils.cs
@@ -26,7 +26,7 @@ namespace Chinook.Src.Utils
                         .Title("[blue bold slowblink]choose your operation[/]".ToUpper())
                         .PageSize(10)
                         .AddChoices(new[] {
-                            MenuOptions.Create, 
+                            MenuOptions.Create,
                             MenuOptions.Read,
                             MenuOptions.Update,
                             MenuOptions.Delete,
@@ -51,7 +51,7 @@ namespace Chinook.Src.Utils
                 }
 
             } while (selection != MenuOptions.Exit);
-            
+
             AnsiConsole.Write(new Markup("[red bold]b[/][green bold]y[/][blue bold]e[/][yellow bold]![/]"));
             AnsiConsole.WriteLine();
         }
@@ -61,12 +61,12 @@ namespace Chinook.Src.Utils
         /// </summary>
         public static void InitiateCreate()
         {
-            try 
-            {   
+            try
+            {
                 bool choice; // declare choice variable
 
-                do 
-                {   
+                do
+                {
                     AnsiConsole.Clear();
 
                     string firstName = AnsiConsole.Ask<string>("ENTER [blue]first name[/]: ");
@@ -93,20 +93,93 @@ namespace Chinook.Src.Utils
                     sb.AppendLine();
 
                     Console.Write(sb);
-                    
+
                     choice = AnsiConsole.Confirm("Confirm and create user?");
                 } while (!choice);
 
                 AnsiConsole.Clear();
-            } 
+            }
             catch (Exception e)
             {
                 AnsiConsole.Write(e.Message);
             }
         }
+
+        public static void ReadMenu()
+        {
+            try
+            {
+                string selection;
+
+                do
+                {
+                    selection = AnsiConsole.Prompt(
+                    new SelectionPrompt<string>()
+                        .Title("[blue bold slowblink]Read customers[/]".ToUpper())
+                        .PageSize(10)
+                        .AddChoices(new[] {
+                            "All customers",
+                            "By limit and offset",
+                            "By country",
+                            "By id",
+                            "By name",
+                            "By net spend",
+                            "Exit"
+                        })
+                );
+
+                    switch (selection)
+                    {
+                        case "All customers":
+                            {
+
+                                // Implement function to get all customers and display in console
+                                switch (selection)
+                                {
+                                    case "All customers":
+                                        DisplayAllCustomers();
+                                        break;
+
+                                }
+
+                                break;
+                            }
+                        case "By limit and offset":
+                            {
+                                break;
+                            }
+                        case "By country":
+                            {
+                                break;
+                            }
+                        case "By id":
+                            {
+                                break;
+                            }
+                        case "By name":
+                            {
+                                break;
+                            }
+                        case "By net spend":
+                            {
+                                break;
+                            }
+                        default: break;
+
+                    }
+
+                } while (selection != "Exit");
+            }
+            catch (Exception e)
+            {
+                AnsiConsole.Write(e.Message);
+            }
+        
+
+        // TODO: Display all customers
         public static void DisplayAllCustomers()
         {
-            var customers = GetAllCustomers();
+                var = "frederik";
 
             var table = new Table()
                 .AddColumn("Id")
@@ -130,90 +203,77 @@ namespace Chinook.Src.Utils
                 );
             }
             AnsiConsole.Render(table);
+
+            
         }
-        public static void ReadMenu()
+
+        // TODO: Display customers with limit and
+
+        // TODO: By Name
+
+        public void DisplayCustomersByName()
         {
-            try
-            {   
-                string selection;
+            string name = AnsiConsole.Ask<string>("Enter the customer name:");
+            var customers = GetCustomersByName(name);
 
-                do
+            if (customers.Any())
+            {
+                var table = new Table()
+                    .AddColumn("Id")
+                    .AddColumn("First Name")
+                    .AddColumn("Last Name")
+                    .AddColumn("Country")
+                    .AddColumn("Postal Code")
+                    .AddColumn("Phone")
+                    .AddColumn("Email");
+
+                foreach (var customer in customers)
                 {
-                    selection = AnsiConsole.Prompt(
-                    new SelectionPrompt<string>()
-                        .Title("[blue bold slowblink]Read customers[/]".ToUpper())
-                        .PageSize(10)
-                        .AddChoices(new[] {
-                            "All customers",
-                            "By limit and offset",
-                            "By country",
-                            "By id",
-                            "By name",
-                            "By net spend",
-                            "Exit"
-                        })
-                );
-
-                switch (selection)
-                {
-                    case "All customers":
-                    {
-
-                                // Implement function to get all customers and display in console
-                                switch (selection)
-                                {
-                                    case "All customers":
-                                        DisplayAllCustomers();
-                                        break;
-                                        
-                                }
-
-                                break;
-                    }
-                    case "By limit and offset":
-                    {
-                        break;
-                    }
-                    case "By country":
-                    {
-                        break;
-                    }
-                    case "By id":
-                    {
-                        break;
-                    }
-                    case "By name":
-                    {
-                        break;
-                    }
-                    case "By net spend":
-                    {
-                        break;
-                    }
-                    default: break;
-                    
+                    table.AddRow(
+                        customer.Id.ToString(),
+                        customer.FirstName,
+                        customer.LastName,
+                        customer.Country,
+                        customer.PostalCode,
+                        customer.PhoneNumber,
+                        customer.Email
+                    );
                 }
 
-                } while (selection != "Exit");
+                AnsiConsole.Render(table);
             }
-            catch (Exception e)
+            else
             {
-                AnsiConsole.Write(e.Message);
+                AnsiConsole.MarkupLine($"[red]No customers found for name: {name}[/]");
             }
         }
 
-        // TODO: Display all customers
-        public static List<Customer> GetAllCustomers()
+
+        // TODO: Customer by ID
+        public void DisplayCustomerById()
         {
-            // Placeholder: Fetch this data from your database.
-            return new List<Customer>
-    {
-        new Customer { Id = 1, FirstName = "John", LastName = "Doe", ... },
-        //... other mock customers
-    };
+            int customerId = AnsiConsole.Ask<int>("Enter the customer ID:");
+            var customer = GetCustomerById(customerId);
+            if (customer != null)
+            {
+                AnsiConsole.MarkupLine($"[bold]{customer.FirstName} {customer.LastName}[/]");
+                AnsiConsole.WriteLine($"Country: {customer.Country}");
+                AnsiConsole.WriteLine($"Postal Code: {customer.PostalCode}");
+                AnsiConsole.WriteLine($"Phone: {customer.PhoneNumber}");
+                AnsiConsole.WriteLine($"Email: {customer.Email}");
+            }
+            else
+            {
+                AnsiConsole.MarkupLine($"[red]No customer found for ID: {customerId}[/]");
+            }
+            // TODO: Pagination
+            public void DisplayPaginatedCustomers()
+            {
+                int limit = AnsiConsole.Ask<int>("Enter limit");
+                int offset = AnsiConsole.Ask<int>("Enter the offset:");
+                var customers = GetCustomersPaginated(limit, offset);
+            }
         }
 
-
-        // TODO: Display customers with limit and 
     }
 }
