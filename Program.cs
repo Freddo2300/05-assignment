520a739412f3389e56283d6e4badc2d265ac14b6
BareMarcP <marc.en.pedersen@gmail.com>
2023-09-22 15:54:59 +0200

    added functions for fetching more data from db

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using Chinook.Src;
+using Chinook.Src.Repositories.CustomerRepo;
+using Chinook.Src.Model;
 
 namespace Chinook
 {
@@ -6,7 +8,24 @@ namespace Chinook
     {
         public static void Main(string[] args)
         {
-            App.Start();
+            //App.Start();
+            ICustomer customerService = new CustomerService(new ChinookContext());
+            // ICollection<Customer> customers = customerService.GetAll();
+            // Customer single_customer = customerService.GetById(1);
+
+
+            // foreach (Customer cust in customers){
+            //     System.Console.WriteLine($"{cust.CustomerId} {cust.FirstName}");
+            // }
+            // System.Console.WriteLine($"{single_customer.CustomerId} {single_customer.FirstName}");
+            // Customer named_customer = customerService.GetByName("Puja");
+            // System.Console.WriteLine($"{named_customer.CustomerId} {named_customer.LastName}");
+            
+            ICollection<Customer> customers = customerService.GetCustomerPage(5, 10);
+
+            foreach (Customer cust in customers){
+                System.Console.WriteLine($"{cust.CustomerId} {cust.FirstName}");
+            }
         }
     }
 }
